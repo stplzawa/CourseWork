3f98acb6b501f84f06f2f1e775cf018e3cbf170d
backtrack <55458936+stplzawa@users.noreply.github.com>
2020-06-23 00:12:12 +0300

    Add files via upload

---
+++ Game/SFML/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/SFML_sources/src/SFML/Network/Ftp.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/Ftp.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/Http.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/Http.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/IpAddress.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/IpAddress.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/Packet.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/Packet.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/Socket.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/Socket.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/SocketSelector.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/SocketSelector.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/TcpListener.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/TcpListener.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/TcpSocket.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/TcpSocket.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/UdpSocket.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/UdpSocket.cpp.obj"
+  "F:/SFML_sources/src/SFML/Network/Win32/SocketImpl.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/Network/CMakeFiles/sfml-network.dir/Win32/SocketImpl.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_NETWORK_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Game/SFML/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/SFML_sources/src/SFML/System/Clock.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Err.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Err.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/FileInputStream.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/FileInputStream.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Lock.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/MemoryInputStream.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/MemoryInputStream.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Mutex.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Mutex.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Sleep.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/String.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/String.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Thread.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Thread.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/ThreadLocal.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/ThreadLocal.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Time.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Win32/ClockImpl.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/ClockImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Win32/MutexImpl.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/MutexImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Win32/SleepImpl.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/SleepImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Win32/ThreadImpl.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/ThreadImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/System/Win32/ThreadLocalImpl.cpp" "F:/SFML_sources/FOR_QT/DEBUG/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/ThreadLocalImpl.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_SYSTEM_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
