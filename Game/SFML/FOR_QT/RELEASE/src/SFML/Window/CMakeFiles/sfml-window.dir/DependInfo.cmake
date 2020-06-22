48e8a5610355d7fedece2b63222ce738e10285a1
backtrack <55458936+stplzawa@users.noreply.github.com>
2020-06-23 00:00:45 +0300

    Add files via upload

---
+++ Game/SFML/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/SFML_sources/src/SFML/Window/Clipboard.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Clipboard.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Context.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Context.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Cursor.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Cursor.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/GlContext.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/GlContext.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/GlResource.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Joystick.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Joystick.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/JoystickManager.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/JoystickManager.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Keyboard.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Keyboard.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Mouse.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Mouse.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Sensor.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Sensor.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/SensorManager.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/SensorManager.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Touch.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Touch.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/VideoMode.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/VideoMode.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/ClipboardImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/ClipboardImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/CursorImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/CursorImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/InputImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/InputImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/JoystickImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/JoystickImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/SensorImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/SensorImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/VideoModeImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/VideoModeImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/WglContext.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WglContext.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/WglExtensions.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WglExtensions.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Win32/WindowImplWin32.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WindowImplWin32.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/Window.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/Window.cpp.obj"
+  "F:/SFML_sources/src/SFML/Window/WindowImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/WindowImpl.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_WINDOW_EXPORTS"
+  "UNICODE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
