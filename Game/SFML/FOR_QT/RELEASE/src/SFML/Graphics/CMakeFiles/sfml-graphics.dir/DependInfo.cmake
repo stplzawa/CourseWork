207b76a68946a43a9e2f56722a3e88a2ff4eb93a
backtrack <55458936+stplzawa@users.noreply.github.com>
2020-06-22 23:58:18 +0300

    Add files via upload

---
+++ Game/SFML/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/SFML_sources/src/SFML/Graphics/BlendMode.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/CircleShape.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/CircleShape.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Color.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Color.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/ConvexShape.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/ConvexShape.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Font.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Font.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/GLCheck.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/GLCheck.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/GLExtensions.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/GLExtensions.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/GLLoader.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/GLLoader.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Glsl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Glsl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Image.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Image.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/ImageLoader.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/ImageLoader.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RectangleShape.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RectangleShape.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RenderStates.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderStates.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RenderTarget.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTarget.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RenderTexture.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTexture.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RenderTextureImpl.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RenderTextureImplDefault.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImplDefault.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RenderTextureImplFBO.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImplFBO.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/RenderWindow.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderWindow.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Shader.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Shader.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Shape.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Shape.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Sprite.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Sprite.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Text.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Text.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Texture.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Texture.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/TextureSaver.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/TextureSaver.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Transform.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Transform.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Transformable.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Transformable.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/Vertex.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/VertexArray.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/VertexArray.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/VertexBuffer.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/VertexBuffer.cpp.obj"
+  "F:/SFML_sources/src/SFML/Graphics/View.cpp" "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/View.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_GRAPHICS_EXPORTS"
+  "STBI_FAILURE_USERMSG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../extlibs/headers/stb_image"
+  "../../extlibs/headers/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake"
+  "F:/SFML_sources/FOR_QT/RELEASE/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
